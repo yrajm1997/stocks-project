d1cd40c39d7f6eba5c636bf7f74dffeae488567f
Yograj Mahawar <89453936+yrajm1997@users.noreply.github.com>
2024-10-25 08:15:44 +0000

    app file updated

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableLambda
 from langchain.schema.runnable import RunnablePassthrough
 from langchain_experimental.utilities import PythonREPL
 from langchain_core.tools import Tool
@@ -22,6 +23,41 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 # Load Model
 llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+
+
+# Create Chain for Query Generation using LCEL (LangChain Expression Language)
+# Build prompt
+template0 = """
+Given the user request below, classify it as either being about `Need SQL`, `Non SQL`, or `Other`.
+If the user request requires some data to be fetched from the database tables then classify it as `Need SQL`.
+If the user request does not requires any data to be fetched from the database but instead need information on what type of data is present inside the database tables then classify it as `Non SQL`.
+If the user request looks out of the context then classify it as `Other`.
+
+The databse has following tables:
+CREATE TABLE IF NOT EXISTS stock_prices (date DATE, open DOUBLE, high DOUBLE, low DOUBLE, close DOUBLE, volume INT, symbol VARCHAR(20));
+CREATE TABLE IF NOT EXISTS income_statement( date DATE, symbol VARCHAR(20), reportedCurrency VARCHAR(20), cik VARCHAR(20), fillingDate DATE, acceptedDate DATE, calendarYear INTEGER, period VARCHAR(20), revenue INTEGER, costOfRevenue INTEGER, grossProfit INTEGER, grossProfitRatio DOUBLE, researchAndDevelopmentExpenses INTEGER, generalAndAdministrativeExpenses INTEGER, sellingAndMarketingExpenses INTEGER, sellingGeneralAndAdministrativeExpenses INTEGER, otherExpenses INTEGER, operatingExpenses INTEGER, costAndExpenses INTEGER, interestIncome INTEGER, interestExpense INTEGER, depreciationAndAmortization INTEGER, ebitda INTEGER, ebitdaratio DOUBLE, operatingIncome INTEGER, operatingIncomeRatio DOUBLE, totalOtherIncomeExpensesNet INTEGER, incomeBeforeTax INTEGER, incomeBeforeTaxRatio DOUBLE, incomeTaxExpense INTEGER, netIncome INTEGER, netIncomeRatio DOUBLE, eps DOUBLE, epsdiluted DOUBLE, weightedAverageShsOut INTEGER, weightedAverageShsOutDil INTEGER );
+CREATE TABLE IF NOT EXISTS balancesheet_statement( date DATE, symbol VARCHAR(20), reportedCurrency VARCHAR(20), cik VARCHAR(20), fillingDate DATE, acceptedDate DATE, calendarYear INTEGER, period VARCHAR(20), cashAndCashEquivalents INTEGER, shortTermInvestments INTEGER, cashAndShortTermInvestments INTEGER, netReceivables INTEGER, inventory INTEGER, otherCurrentAssets INTEGER, totalCurrentAssets INTEGER, propertyPlantEquipmentNet INTEGER, goodwill INTEGER, intangibleAssets INTEGER, goodwillAndIntangibleAssets INTEGER, longTermInvestments INTEGER, taxAssets INTEGER, otherNonCurrentAssets INTEGER, totalNonCurrentAssets INTEGER, otherAssets INTEGER, totalAssets INTEGER, accountPayables INTEGER, shortTermDebt INTEGER, taxPayables INTEGER, deferredRevenue INTEGER, otherCurrentLiabilities INTEGER, totalCurrentLiabilities INTEGER, longTermDebt INTEGER, deferredRevenueNonCurrent INTEGER, deferredTaxLiabilitiesNonCurrent INTEGER, otherNonCurrentLiabilities INTEGER, totalNonCurrentLiabilities INTEGER, otherLiabilities INTEGER, capitalLeaseObligations INTEGER, totalLiabilities VARCHAR(20), preferredStock VARCHAR(20), commonStock VARCHAR(20), retainedEarnings VARCHAR(20), accumulatedOtherComprehensiveIncomeLoss VARCHAR(20), othertotalStockholdersEquity VARCHAR(20), totalStockholdersEquity VARCHAR(20), totalEquity VARCHAR(20), totalLiabilitiesAndStockholdersEquity VARCHAR(20), minorityInterest VARCHAR(20), totalLiabilitiesAndTotalEquity VARCHAR(20), totalInvestments VARCHAR(20), totalDebt VARCHAR(20), netDebt VARCHAR(20) ); 
+CREATE TABLE IF NOT EXISTS cashflow_statement( date DATE, symbol VARCHAR(20), reportedCurrency VARCHAR(20), cik VARCHAR(20), fillingDate DATE, acceptedDate DATE, calendarYear INTEGER, period VARCHAR(20), netIncome INTEGER, depreciationAndAmortization INTEGER, deferredIncomeTax INTEGER, stockBasedCompensation INTEGER, changeInWorkingCapital INTEGER, accountsReceivables INTEGER, inventory INTEGER, accountsPayables INTEGER, otherWorkingCapital INTEGER, otherNonCashItems INTEGER, netCashProvidedByOperatingActivities INTEGER, investmentsInPropertyPlantAndEquipment INTEGER, acquisitionsNet INTEGER, purchasesOfInvestments INTEGER, salesMaturitiesOfInvestments INTEGER, otherInvestingActivites INTEGER, netCashUsedForInvestingActivites INTEGER, debtRepayment INTEGER, commonStockIssued INTEGER, commonStockRepurchased INTEGER, dividendsPaid INTEGER, otherFinancingActivites INTEGER, netCashUsedProvidedByFinancingActivities INTEGER, effectOfForexChangesOnCash INTEGER, netChangeInCash INTEGER, cashAtEndOfPeriod INTEGER, cashAtBeginningOfPeriod INTEGER, operatingCashFlow INTEGER, capitalExpenditure INTEGER, freeCashFlow INTEGER ); 
+The 'symbol' column in each table contains the companies names in capital letters.
+
+Do not respond with more than two words.
+
+Request: {request}
+Classification:
+"""
+
+PROMPT0 = PromptTemplate(input_variables=["request"], template=template0)
+
+# Classification Chain
+clf_chain = (PROMPT0
+             | llm
+             | StrOutputParser()       # to get output in a more usable format
+             )
+
+# # Classify query
+# response0 = clf_chain.invoke({"request": "Need open, high prices for any ten Wipro records"})
+# print(response0)
+
 
 # Create Chain for Query Generation using LCEL (LangChain Expression Language)
 # Build prompt
@@ -54,13 +90,13 @@ SQLQuery:
 PROMPT1 = PromptTemplate(input_variables=["request"], template=template1)
 
 # Query Generation Chain - created using LCEL (LangChain Expression Language)
-chain1 = (PROMPT1
-          | llm
-          | StrOutputParser()       # to get output in a more usable format
-          )
+sql_chain = (PROMPT1
+             | llm
+             | StrOutputParser()       # to get output in a more usable format
+             )
 
 ## Generate sql query
-# response1 = chain1.invoke({"request": "Need open, high prices for any ten Wipro records"})
+# response1 = sql_chain.invoke({"request": "Need open, high prices for any ten Wipro records"})
 # print(response1)
 
 
@@ -90,11 +126,11 @@ Generate code:
 
 PROMPT2 = PromptTemplate(input_variables=["request_plus_sqlquery"], template=template2)
 
-# Query Generation Chain - created using LCEL (LangChain Expression Language)
-chain2 = (PROMPT2
-          | llm
-          | StrOutputParser()       # to get output in a more usable format
-          )
+# Code Generation Chain - created using LCEL (LangChain Expression Language)
+code_chain = (PROMPT2
+              | llm
+              | StrOutputParser()       # to get output in a more usable format
+              )
 
 
 # Create Chain for Generating Suggestions
@@ -119,23 +155,52 @@ Generate suggestion:
 PROMPT3 = PromptTemplate(input_variables=["request"], template=template3)
 
 # Suggestion Generation Chain - created using LCEL (LangChain Expression Language)
-chain3 = (PROMPT3
-          | llm
-          | StrOutputParser()       # to get output in a more usable format
-          )
-
-# Final Chain
-final_chain = chain1 | chain2
+sug_chain = (PROMPT3
+             | llm
+             | StrOutputParser()       # to get output in a more usable format
+             )
 
 
-# Create UI using Chainlit
+# Create Chain for Generating Response for General queries about the data stored in DB
+# Build prompt
+template4 = """
+Use the following user request and database details to generate appropriate response describing the data stored inside the database.
+Response should not be more than 10 lines.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+SQLite database has following tables:
+CREATE TABLE IF NOT EXISTS stock_prices (date DATE, open DOUBLE, high DOUBLE, low DOUBLE, close DOUBLE, volume INT, symbol VARCHAR(20));
+CREATE TABLE IF NOT EXISTS income_statement( date DATE, symbol VARCHAR(20), reportedCurrency VARCHAR(20), cik VARCHAR(20), fillingDate DATE, acceptedDate DATE, calendarYear INTEGER, period VARCHAR(20), revenue INTEGER, costOfRevenue INTEGER, grossProfit INTEGER, grossProfitRatio DOUBLE, researchAndDevelopmentExpenses INTEGER, generalAndAdministrativeExpenses INTEGER, sellingAndMarketingExpenses INTEGER, sellingGeneralAndAdministrativeExpenses INTEGER, otherExpenses INTEGER, operatingExpenses INTEGER, costAndExpenses INTEGER, interestIncome INTEGER, interestExpense INTEGER, depreciationAndAmortization INTEGER, ebitda INTEGER, ebitdaratio DOUBLE, operatingIncome INTEGER, operatingIncomeRatio DOUBLE, totalOtherIncomeExpensesNet INTEGER, incomeBeforeTax INTEGER, incomeBeforeTaxRatio DOUBLE, incomeTaxExpense INTEGER, netIncome INTEGER, netIncomeRatio DOUBLE, eps DOUBLE, epsdiluted DOUBLE, weightedAverageShsOut INTEGER, weightedAverageShsOutDil INTEGER );
+CREATE TABLE IF NOT EXISTS balancesheet_statement( date DATE, symbol VARCHAR(20), reportedCurrency VARCHAR(20), cik VARCHAR(20), fillingDate DATE, acceptedDate DATE, calendarYear INTEGER, period VARCHAR(20), cashAndCashEquivalents INTEGER, shortTermInvestments INTEGER, cashAndShortTermInvestments INTEGER, netReceivables INTEGER, inventory INTEGER, otherCurrentAssets INTEGER, totalCurrentAssets INTEGER, propertyPlantEquipmentNet INTEGER, goodwill INTEGER, intangibleAssets INTEGER, goodwillAndIntangibleAssets INTEGER, longTermInvestments INTEGER, taxAssets INTEGER, otherNonCurrentAssets INTEGER, totalNonCurrentAssets INTEGER, otherAssets INTEGER, totalAssets INTEGER, accountPayables INTEGER, shortTermDebt INTEGER, taxPayables INTEGER, deferredRevenue INTEGER, otherCurrentLiabilities INTEGER, totalCurrentLiabilities INTEGER, longTermDebt INTEGER, deferredRevenueNonCurrent INTEGER, deferredTaxLiabilitiesNonCurrent INTEGER, otherNonCurrentLiabilities INTEGER, totalNonCurrentLiabilities INTEGER, otherLiabilities INTEGER, capitalLeaseObligations INTEGER, totalLiabilities VARCHAR(20), preferredStock VARCHAR(20), commonStock VARCHAR(20), retainedEarnings VARCHAR(20), accumulatedOtherComprehensiveIncomeLoss VARCHAR(20), othertotalStockholdersEquity VARCHAR(20), totalStockholdersEquity VARCHAR(20), totalEquity VARCHAR(20), totalLiabilitiesAndStockholdersEquity VARCHAR(20), minorityInterest VARCHAR(20), totalLiabilitiesAndTotalEquity VARCHAR(20), totalInvestments VARCHAR(20), totalDebt VARCHAR(20), netDebt VARCHAR(20) ); 
+CREATE TABLE IF NOT EXISTS cashflow_statement( date DATE, symbol VARCHAR(20), reportedCurrency VARCHAR(20), cik VARCHAR(20), fillingDate DATE, acceptedDate DATE, calendarYear INTEGER, period VARCHAR(20), netIncome INTEGER, depreciationAndAmortization INTEGER, deferredIncomeTax INTEGER, stockBasedCompensation INTEGER, changeInWorkingCapital INTEGER, accountsReceivables INTEGER, inventory INTEGER, accountsPayables INTEGER, otherWorkingCapital INTEGER, otherNonCashItems INTEGER, netCashProvidedByOperatingActivities INTEGER, investmentsInPropertyPlantAndEquipment INTEGER, acquisitionsNet INTEGER, purchasesOfInvestments INTEGER, salesMaturitiesOfInvestments INTEGER, otherInvestingActivites INTEGER, netCashUsedForInvestingActivites INTEGER, debtRepayment INTEGER, commonStockIssued INTEGER, commonStockRepurchased INTEGER, dividendsPaid INTEGER, otherFinancingActivites INTEGER, netCashUsedProvidedByFinancingActivities INTEGER, effectOfForexChangesOnCash INTEGER, netChangeInCash INTEGER, cashAtEndOfPeriod INTEGER, cashAtBeginningOfPeriod INTEGER, operatingCashFlow INTEGER, capitalExpenditure INTEGER, freeCashFlow INTEGER ); 
+The 'symbol' column in each table contains the companies names in capital letters.
+
+{request}
+
+Generate response:
+"""
+
+PROMPT4 = PromptTemplate(input_variables=["request"], template=template4)
+
+# Suggestion Generation Chain - created using LCEL (LangChain Expression Language)
+gnrl_chain = (PROMPT4
+              | llm
+              | StrOutputParser()       # to get output in a more usable format
+              )
+
+
+# Club SQL + Code generation Chain
+sql_code_chain = sql_chain | code_chain
+
+
+### Create UI using Chainlit
 import chainlit as cl
 
-@cl.on_chat_start
+@cl.on_chat_start            # for actions to happen when the chat starts
 async def main():
     await cl.Message(content=f"Welcome to Stock Prices Insights Application").send()
 
-@cl.on_message
+@cl.on_message               # for actions to happen whenever user enters a message
 async def main(message: cl.Message):
 
     # print(f"User Input: {message.content}")
@@ -146,20 +211,26 @@ async def main(message: cl.Message):
         if os.path.isfile(file_path) and filename != '__init__.py':
             os.remove(file_path)  # Remove file
 
-    # Generate code for insights
-    code_response = final_chain.invoke({"request": message.content})
-    # # print(code_response)
-    # # print("="*50)
-    
-    # Execute code
-    output = repl_tool.run(code_response)
-    # print(output)
+    # Route the user request to necessary chain
+    clf_label = clf_chain.invoke({"request": message.content})
+
+    if "need sql" in clf_label.lower():
+        # Generate code for insights
+        code_response = sql_code_chain.invoke({"request": message.content})
+        # # print(code_response)
+        # Execute code
+        output = repl_tool.run(code_response)
+        # print(output)
+    elif "non sql" in clf_label.lower():
+        output = gnrl_chain.invoke({"request": message.content})
+    else:
+        output = "The request is out of context."
 
     # Generate suggestions
-    suggest = chain3.invoke({"request": message.content})
+    suggest = sug_chain.invoke({"request": message.content})
     
-    # Send the plot to the chat
-    if len(os.listdir('figures')) != 0:
+    # If image is present inside 'figures' directory then Send the plot image to the chat
+    if len(os.listdir('figures')) > 1:
         for imgfile in os.listdir('figures'): 
             print(imgfile)
             if os.path.isfile(os.path.join('figures', imgfile)) and imgfile != '__init__.py':
@@ -173,4 +244,4 @@ async def main(message: cl.Message):
     else:
         # Send response
         await cl.Message(content=f"Response: \n{output}",).send()
-        await cl.Message(content=f"Further suggestion: \n{suggest}",).send()
+        await cl.Message(content=f"Further suggestions: \n{suggest}",).send()
